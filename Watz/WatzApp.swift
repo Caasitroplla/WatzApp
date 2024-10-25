d14c1a859a562124e5520f431b6ab36468c270d5
caasitroplla <caasitroplla@icloud.com>
2024-10-25 17:17:36 +0100

    Initial Commit

---
+++ Watz/WatzApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatzApp.swift
+//  Watz
+//
+//  Created by Isaac Allport on 25/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct WatzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
